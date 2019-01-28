43fc19a10bbbbd95664ab80e73cb791523f646e5
takana303 <msdf303@live.jp>
2019-01-29 02:39:09 +0900

    Update main_editing.py

--- main_editing.py
+++ main_editing.py
@@ -28,6 +28,19 @@ from oauth2client import tools
 
 ### Youtube section ###
 
+'''
+JSON_FILE
+{"installed":{
+	"client_id":"<FILLIN>",
+	"project_id":"<FILLIN>",
+	"auth_uri":"https://accounts.google.com/o/oauth2/auth",
+	"token_uri":"https://www.googleapis.com/oauth2/v3/token",
+	"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+	"client_secret":"<FILLIN>",
+	"redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
+}}
+'''
+
 def _get_authenticated_service(JSON_FILE):
 	flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=SCOPE)
 
